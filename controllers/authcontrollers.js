4bde96deadd8ec807e5266f5a92bf16e454d3055
sahu-virendra-1908 <virendrashivsahu@gmail.com>
2025-01-16 18:35:14 +0530

    change jwt token

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -1,37 +1,3 @@
-// const usermodel = require('../models/User');
-// const bcrypt = require('bcryptjs');
-
-// const register = async (req, res) => {
-//     try {
-//         const { email, password, name } = req.body;
-//         if (!email || !password || !name) {
-//             return res.status(400).json({ error: 'Please fill all the fields' });
-//         }
-
-//         const ExistingUser = await usermodel.findOne({ email });
-//         if (ExistingUser) {
-//             return res.status(400).json({ error: 'User already exists' });
-//         }
-// const verificationcode = Math.floor(Math.random() * 1000000);
-//         const hashpassword = await bcrypt.hash(password, 10);
-//         const user = new usermodel({
-//             name,
-//             email,
-//             password: hashpassword,
-//             verificationcode
-//         });
-//         await user.save();
-//         return res.status(200).json({ message: 'User registered successfully' });
-
-//     } catch (error) {
-//         console.log('Error in register API :', error);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// }
-
-// module.exports = { register };
-
-
 const { generateOTP, isOTPExpired } = require('../services/otp.service');
 const { sendOTPEmail } = require('../services/Email.confiq.js');
 const jwt = require('jsonwebtoken');
@@ -127,7 +93,7 @@ const login = async (req, res) => {
         return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '48h' });
+    const token = jwt.sign({ id: user._id }, "virendra", { expiresIn: '48h' });
     res.status(200).json({ message: 'Login successful', token });
 };
 
